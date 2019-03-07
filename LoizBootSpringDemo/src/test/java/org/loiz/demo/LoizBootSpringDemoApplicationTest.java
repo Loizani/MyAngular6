e39236bd31d7f88bec39e2c546c39d76bf9d1076
Loizani <Loizani@aol.com>
2019-03-07 17:27:54 +0100

    Sprib Boot Rest VO Users

---
+++ LoizBootSpringDemo/src/test/java/org/loiz/demo/LoizBootSpringDemoApplicationTest.java
@@ -0,0 +1,15 @@
+package org.loiz.demo;
+
+import org.junit.Test;
+
+
+public class LoizBootSpringDemoApplicationTest 
+{
+	@Test
+	public void testDummy()
+	{
+		boolean bol = true; 
+		org.junit.Assert.assertTrue( bol==true );
+	}
+
+}
