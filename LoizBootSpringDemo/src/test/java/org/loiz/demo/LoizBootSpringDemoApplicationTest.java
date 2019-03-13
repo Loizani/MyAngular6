da8204e1ddb6f0d9a59f0579f609222d465387b2
Loizani <Loizani@aol.com>
2019-03-13 22:52:01 +0100

    Version avec la partie Angular 7 : ajout d'autres tests

--- LoizBootSpringDemo/src/test/java/org/loiz/demo/LoizBootSpringDemoApplicationTest.java
+++ LoizBootSpringDemo/src/test/java/org/loiz/demo/LoizBootSpringDemoApplicationTest.java
@@ -6,10 +6,38 @@ import org.junit.Test;
 public class LoizBootSpringDemoApplicationTest 
 {
 	@Test
-	public void testDummy()
+	public void testDummy1()
 	{
 		boolean bol = true; 
-		org.junit.Assert.assertTrue( bol==true );
+		org.junit.Assert.assertTrue( bol==true );		
+	}
+	
+	@Test
+	public void testDummy2()
+	{
+		boolean bol = true; 
+		org.junit.Assert.assertTrue( bol==true );		
+	}
+	
+	@Test
+	public void testDummy3()
+	{
+		boolean bol = true; 
+		org.junit.Assert.assertTrue( bol==true );		
+	}
+	
+	@Test
+	public void testDummy4()
+	{
+		boolean bol = true; 
+		org.junit.Assert.assertTrue( bol==true );		
+	}
+	
+	@Test
+	public void testDummy5()
+	{
+		boolean bol = true; 
+		org.junit.Assert.assertTrue( bol==true );		
 	}
 
 }
